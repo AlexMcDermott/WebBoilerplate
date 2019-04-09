0ad01419e9365d06fadf82ba26cc015998eb8d67
Alex McDermott <alexjmcd2202@gmail.com>
2019-04-09 16:19:29 +1000

    Added p5 as canvas library

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,13 @@
-import Canvas from './canvas';
+import p5js from 'p5';
 
-const cnv = new Canvas();
+function sketch(p5: p5js) {
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+  };
 
-function draw() {
-  cnv.background([255, 255, 255]);
+  p5.draw = () => {
+    p5.background(0);
+  };
 }
 
-cnv.start(draw);
+const project = new p5js(sketch);
